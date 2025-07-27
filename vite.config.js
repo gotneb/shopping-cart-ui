d696f36822d7612208945dd7ea4d01f9abb67476
Gabriel Bento <gabrielorigenstdb@gmail.com>
2025-07-27 15:47:21 -0300

    feat: setup proxy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        // It proxies every request to localhost, and remove the 'api'
+        target: 'http://localhost:5000',
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        changeOrigin: true,
+      }
+    }
+  }
 })
