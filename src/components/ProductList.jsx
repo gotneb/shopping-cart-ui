fff9f5e715c04bf72641d7a06bbaa033f9371e6a
Gabriel Bento <gabrielorigenstdb@gmail.com>
2025-07-27 16:08:24 -0300

    feat: add useProducts context hook

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,19 @@
 import ProductCard from "./ProductCard";
+import { useProducts } from "../context/ProductContext";
 
-const ProductsList = ({ products }) => {
+const ProductsList = () => {
+  // Approach without using custom hook 'useProducts'
+  // const { products, loading, error } = useContext(ProductContext);
+  const { products, loading, error } = useProducts();
+
   return (
     // Very useful. It uses grid from Tailwind. Logic behind:
     // grid-cols-1: for mobile devices
     // sm:grid-cols-2: for small screens, has 2 columns
     // md:grid-cols-3: for medium screens, has 3 columns
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+      {loading && <p>Loading...</p>}
+      {error && <div className="error">{error}</div>}
       {products.map((p) => (
         <ProductCard key={p.id} product={p} />
       ))}
