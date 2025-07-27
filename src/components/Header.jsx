9f8d81e6c0072cf23c7bf6fb350155861611d418
Gabriel Bento <gabrielorigenstdb@gmail.com>
2025-07-27 18:07:01 -0300

    feat: show cart items list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,66 @@
 import { useCart } from "../context/CartContext";
 import { FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
 
 const Header = () => {
+  const [showDropDown, setShowDropDown] = useState(false);
+
   const { cart } = useCart();
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
+  const totalPrice = cart
+    .reduce((acc, item) => acc + item.price * item.qty, 0)
+    .toFixed(2);
 
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold text-blue-600">Plaza</h1>
 
       <div className="relative">
-        <FaShoppingCart className="text-2xl text-gray-700" />
-        {itemCount > 0 && (
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-            {itemCount}
-          </span>
+        <button
+          onClick={() => setShowDropDown(!showDropDown)}
+          className="cursor-pointer"
+        >
+          <FaShoppingCart className="text-2xl text-gray-700" />
+          {itemCount > 0 && (
+            <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+              {itemCount}
+            </span>
+          )}
+        </button>
+
+        {showDropDown && (
+          // The parent is relative. So it arranges this div relative to its parent. 
+          <div className="absolute right-0 mt-2 w-80 bg-white border rounded shadow-lg z-50">
+            <div className="p-4">
+              <h2 className="font-semibold text-lg mb-2">Cart Items</h2>
+              {cart.length === 0 ? (
+                <p className="text-gray-500 text-sm">Cart is empty.</p>
+              ) : (
+                <>
+                  <ul className="max-h-60 overflow-y-auto divide-y divide-gray-200">
+                    {cart.map((product) => (
+                      <li
+                        key={product.id}
+                        className="flex justify-between items-center py-2"
+                      >
+                        <div>
+                          <p className="font-semibold">{product.name}</p>
+                          <p className="text-sm text-gray-500">
+                            {product.qty} x R${product.price}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <div className="mt-4 flex justify-between font-semibold">
+                    <span>Total:</span>
+                    <span>R${totalPrice}</span>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
         )}
       </div>
     </header>
